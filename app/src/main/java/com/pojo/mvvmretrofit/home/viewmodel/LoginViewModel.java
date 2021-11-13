99d2f9191d15d16a3df281a4a5b15d8f499e58c1
murali <murali.e@sosaley.in>
2021-11-13 13:52:15 +0530

    RecyclerView Commit

--- app/src/main/java/com/pojo/mvvmretrofit/home/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/pojo/mvvmretrofit/home/viewmodel/LoginViewModel.java
@@ -1,7 +1,10 @@
 package com.pojo.mvvmretrofit.home.viewmodel;
 
+import android.app.Application;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -11,18 +14,27 @@ import com.pojo.mvvmretrofit.repository.Repository;
 
 import retrofit2.Response;
 
-public class LoginViewModel extends ViewModel {
+public class LoginViewModel extends AndroidViewModel {
+
+
 
     Repository repository=new Repository();
+
 
     public MutableLiveData<String> phoneNo = new MutableLiveData<>();
     public MutableLiveData<String> phoneNoError = new MutableLiveData<>();
     public MutableLiveData<String> password = new MutableLiveData<>();
 
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+    }
+
     public LiveData<Response> login() {
         LoginRequestModel requestModel = new LoginRequestModel();
         requestModel.setMobileNumber(phoneNo.getValue());
         requestModel.setPassword(password.getValue());
+        System.out.println("DataEntered"+requestModel.getMobileNumber()+" "+requestModel.getPassword());
+
         return repository.loginUser(requestModel);
     }
 
