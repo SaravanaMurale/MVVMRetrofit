24c5491cfcc1c38f6e1e9e172a44a665cdae37c0
murali <murali.e@sosaley.in>
2021-11-30 18:57:02 +0530

    Navigation Component Commit

--- app/src/main/java/com/pojo/mvvmretrofit/navigationcomponent/MainFragment.java
+++ app/src/main/java/com/pojo/mvvmretrofit/navigationcomponent/MainFragment.java
@@ -3,10 +3,12 @@ package com.pojo.mvvmretrofit.navigationcomponent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.pojo.mvvmretrofit.R;
 
@@ -14,6 +16,7 @@ import com.pojo.mvvmretrofit.R;
 public class MainFragment extends Fragment {
 
 
+    Button btnTransaction,btnSendMoney,btnViewBalance;
 
     public MainFragment() {
         // Required empty public constructor
@@ -30,6 +33,40 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_main, container, false);
+
+        btnTransaction=view.findViewById(R.id.viewTransaction);
+        btnSendMoney=view.findViewById(R.id.sendMoney);
+        btnViewBalance=view.findViewById(R.id.viewBalance);
+
+
+        btnTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Navigation.findNavController(v).navigate(R.id.action_mainFragment2_to_transactionFragment2);
+
+            }
+        });
+
+        btnSendMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Navigation.findNavController(v).navigate(R.id.action_mainFragment2_to_sendMoneyFragment);
+
+            }
+        });
+
+        btnViewBalance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Navigation.findNavController(v).navigate(R.id.action_mainFragment2_to_viewBalanceFragment);
+            }
+        });
+
+        return view ;
     }
 }
--- app/src/main/java/com/pojo/mvvmretrofit/navigationcomponent/NavigationActivity.java
+++ app/src/main/java/com/pojo/mvvmretrofit/navigationcomponent/NavigationActivity.java
@@ -1,6 +1,9 @@
 package com.pojo.mvvmretrofit.navigationcomponent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -8,9 +11,15 @@ import com.pojo.mvvmretrofit.R;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment_container_main);
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
     }
 }
